e8ba47f31e54c1b32b3cf57400502d211d89a376
Kshitij-Halmare <horizon29042004@gmail.com>
2025-05-09 16:29:03 +0530

    Signin Page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Layout from './Component/Layout';
 import Home from './Pages/Home';
+import Signin from './Pages/Signin';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index  element={<Home />} />
+        <Route path="signin" element={<Signin/>}></Route>
       </Route>
     </Routes>
   );
