63dafcc6edf04472ecb50dd74c22eb80f8beeaad
Kshitij-Halmare <horizon29042004@gmail.com>
2025-05-09 16:43:40 +0530

    Signup Page UI

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import Layout from './Component/Layout';
 import Home from './Pages/Home';
 import Signin from './Pages/Signin';
+import Signup from './Pages/Signup';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Route path="/" element={<Layout />}>
         <Route index  element={<Home />} />
         <Route path="signin" element={<Signin/>}></Route>
+        <Route path="signup" element={<Signup/>}></Route>
       </Route>
     </Routes>
   );
